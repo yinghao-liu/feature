6b91dfa98a66f2b9cb5d5ff3562f1e443655706e
lyh <lyh@inc>
2016-11-02 19:29:36 -0700

    printf str with specified length

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2016 francis_hao <francis_hao@126.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or
+ * NON INFRINGEMENT.  See the GNU General Public License for more
+ * details.
+ *
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	printf("%.*s\n", 5, "123456789");
+	return 0;
+}
