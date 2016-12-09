00a6079928c393cd40b1d6b2f58dec76425e4bee
lyh <lyh@inc>
2016-12-09 01:28:19 -0800

    mac

--- main.c
+++ main.c
@@ -15,8 +15,56 @@
  */
 
 #include <stdio.h>
+#include <arpa/inet.h>
 int main(void)
 {
-	printf("%.*s\n", 5, "123456789");
+	unsigned int gateip = 0x010ba8c0;
+	unsigned char aa[20]={0};
+	unsigned char gateway_mac[20]={0};
+
+	char buff[256]="192.168.11.1	0x1	0x2                    	00:1d:a1:7a:a7:bd	* eth2";
+    char gateway[16] = {0};
+    int iret = 0; 
+    int index = 0;
+    char *pos = 0;
+
+if(iret == 0)
+    {
+        pos = buff + 41;
+        while(index < 6 && buff + 58)
+        {
+            if(pos[0] >= 'A' && pos[0] <= 'Z')
+            {
+                pos[0] -= 0x37;
+            }
+            else if(pos[0] >= '0' && pos[0] <= '9')
+            {
+                pos[0] -= 0x30;
+            }
+            else
+            {
+                pos++;
+                continue;
+            }
+
+            if(pos[1] >= 'A' && pos[1] <= 'Z')
+            {
+                pos[1] -= 0x37;
+            }
+            else if(pos[1] >= '0' && pos[1] <= '9')
+            {
+                pos[1] -= 0x30;
+            }
+
+            gateway_mac[index++] = (pos[0]<<4)|(pos[1]&0xf);
+
+            pos += 2;
+        }
+    }
+    printf("gateway_mac %02x:%02x:%02x:%02x:%02x:%02x:\n",gateway_mac[0],gateway_mac[1],gateway_mac[2],gateway_mac[3],gateway_mac[4],gateway_mac[5]);
+
+
+//	inet_ntop(AF_INET, (void *)&gateip, aa, 16);
+	printf("%s\n",aa);
 	return 0;
 }
