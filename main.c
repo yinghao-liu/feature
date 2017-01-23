c5cc2ce2782985fc8ae252f6e0bc84601fe33e19
lyh <lyh@inc>
2017-01-22 18:09:47 -0800

    mmap

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 francis_hao <francis_hao@126.com>
+ * Copyright (C) 2017 francis_hao <francis_hao@126.com>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,59 +16,41 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <string.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <time.h>
-#include <sys/shm.h>
-#include "common.h"
+#include <signal.h>
+#include <sys/mman.h>
+void signal_handle(int no)
+{
+	pid_t pid;
+	pid = getpid();	
+	printf("pid %u receive signal %u\n",pid,no);
+}
 
 int main(int argc,char **argv)
 {
-	int shmid;
-	key_t ipckey;
-	//char *shmaddr = NULL;
-	shm_data *shmaddr = NULL;	//struct pointer
-	int shm_size = sizeof (shm_data);
-	ipckey = SYS_IPCSHM_KEY;
-	shmid = shmget(ipckey, shm_size, IPC_CREAT | 0666);
-
-	if(shmid == -1)
-    {
-        perror("creat shm error");
-        return -1;
-    }
-
-    shmaddr = (shm_data *)shmat(shmid,0,0);
-    if(shmaddr == (shm_data *)(~0))
-    {
-        perror("attach shm error!");
-        return -1;
-    }
-	memset(shmaddr, 0x0, shm_size);
-	shmaddr->parray = &shmaddr->array[0];
-	shmaddr->array[0]=0;
-	shmaddr->array[1]=1;
-	shmaddr->array[2]=2;
-	shmaddr->array[3]=3;
-	shmaddr->array[4]=4;
-	shmaddr->array[5]=5;
-	shmaddr->array[6]=6;
-	shmaddr->array[7]=7;
-	printf("main shmaddr->parray point to 0 is %p\n",shmaddr->parray);
-	printf("main index %u addr is %p\n",shmaddr->array[0],&shmaddr->array[0]);
-	printf("main index %u addr is %p\n",shmaddr->array[1],&shmaddr->array[1]);
-	printf("main index %u addr is %p\n",shmaddr->array[2],&shmaddr->array[2]);
-	printf("main index %u addr is %p\n",shmaddr->array[3],&shmaddr->array[3]);
-	printf("main index %u addr is %p\n",shmaddr->array[4],&shmaddr->array[4]);
-	printf("main index %u addr is %p\n",shmaddr->array[5],&shmaddr->array[5]);
-	printf("main index %u addr is %p\n",shmaddr->array[6],&shmaddr->array[6]);
-	printf("main index %u addr is %p\n",shmaddr->array[7],&shmaddr->array[7]);
-	shmdt(shmaddr);
-	sleep(8);
-	//shmctl(shmid,IPC_RMID,0);
-	exit(0);
+	char tick;
+	pid_t pdd;
+	char *mm;
+	mm = (char *)mmap(NULL,sizeof (char),PROT_READ|PROT_WRITE,MAP_SHARED|MAP_ANONYMOUS,-1,0);
+	*mm=1;
+	printf("memory is %u\n",*mm);
+	//signal(SIGINT,signal_handle);
+	signal(SIGTERM,signal_handle);
+	for (tick=0;tick<5;tick++){
+		pdd = fork();
+		if (-1 == pdd ){
+			perror("fork");
+		}else if (0 == pdd){	//child process
+			printf("child #%u started pid is %u\n",tick,getpid());
+			*mm=tick;
+			printf("child #%u pid is %u  memory is %u\n",tick,getpid(),*mm);
+			while (1);
+		}else{	//father
+			continue;	
+		}
+	}
+	printf("child #%u pid is %u  memory is %u\n",tick,getpid(),*mm);
+	while (1);
 	return 0;
 }
