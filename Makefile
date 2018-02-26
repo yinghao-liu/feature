f1e3ed2a221588795898c79a6878e7d254cc5e3e
yinghao-liu <yinghao_liu@163.com>
2018-02-26 18:00:04 +0800

    解析文件内容并存储到map

--- Makefile
+++ Makefile
@@ -1,17 +1,17 @@
 MAJOR = 2
 MINOR = 0
-REVISION = 1
-NAME  = curl_test
+REVISION = 2
+NAME  = parser_file
 
 BUILD = $(shell date +%c)
 VERSION = "$(NAME) version $(MAJOR).$(MINOR).$(REVISION)\ncompiled $(BUILD)"
 
 CPPFLAGS= -c -g
-LDFLAGS= -lpthread -lcurl
+#LDFLAGS= -lpthread -lcurl
 CC=g++
 all:main
 
 main.o:CXXFLAGS += -D'VERSION=$(VERSION)' -D'NAME="$(NAME)"'
-main:main.o curl_builder.o
+main:main.o
 clean:
 	@rm -rf *.o *.so main
--- main.cpp
+++ main.cpp
@@ -1,29 +1,65 @@
-#include "curl_builder.h"
-#include <curl/curl.h>
-#include <string>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
 #include <stdio.h>
+#include <map>
 #include <iostream>
 using namespace std;
 
-/********************version***************************/
-inline void show_version(void)
+#define MAPFILE      "mapfile"
+int parser_companyfile(map<string, string> &apmac_map)
 {
-	    /*VERSION is a macro, defined in Makefile*/
-	    printf("%s\n",VERSION);
+	struct stat modify;
+	static time_t last_modify=0;
+	if (-1 == stat(MAPFILE, &modify)){
+		perror("stat MAPFILE");
+		return -1;
+	}
+	if (last_modify == modify.st_mtime){
+		printf("not been modified\n");
+		return 0;
+	}
+
+	last_modify = modify.st_mtime;
+
+	char line[4096]={0};
+	char tmp[2][125]={0};
+	FILE *fd;
+	fd = fopen(MAPFILE, "r");
+	if (NULL == fd){
+		perror("open MAPFILE");
+		return -1; 
+	}   
+	apmac_map.clear();
+	string s1,s2;
+	while(NULL != fgets(line, sizeof (line), fd)){
+		sscanf(line, "%*[^\t]\t%[^\t]\t%[^\t]", tmp[0], tmp[1]);
+		if (0 == strlen(tmp[0])){
+			continue;
+		}   
+		if (0 == strlen(tmp[1])){
+			strcpy(tmp[1], "000000");
+		}
+		s1 = tmp[0];
+		s2 = tmp[1];
+		apmac_map.insert(make_pair(s1, s2));
+		memset(tmp[0], 0, sizeof (tmp[0]));
+		memset(tmp[1], 0, sizeof (tmp[1]));
+	}   
+	fclose(fd);
+	return 0;
+
 }
 
 int main(void)
 {
-	//const char *uri = "https://www.bing.com";
-	const char *uri = "http://www.qq.com";
-	curl_t curl;
-	if (-1 == curl.init()){
-		return -1;
+	map<string, string> mapp;
+	parser_companyfile(mapp);
+	parser_companyfile(mapp);
+	map<string, string>::iterator iter;
+	for (iter=mapp.begin(); iter!=mapp.end(); iter++){
+		cout<< iter->first << " => " << iter->second << endl;
 	}
-	int ret=-1;
-	ret = curl.get_url(uri);
-	printf("main ret is %d\n", ret);
 	return 0;
 }
-
