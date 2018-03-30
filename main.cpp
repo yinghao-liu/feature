a9fe2beca63e7c40cc8a1de6c3709675a766cd4f
yinghao-liu <yinghao_liu@163.com>
2018-03-30 14:07:31 +0800

    c environ 环境指针

--- main.cpp
+++ main.cpp
@@ -1,47 +1,10 @@
-#include <iostream>
-#include <thread>
-#include <string>
-#include <unistd.h>
-#include <mutex>
-#include <condition_variable>
-
-using namespace std;
-mutex mtx;
-condition_variable cv;
-int flag=0;
-
-void print_id(int id)
-{
-	unique_lock<mutex> lock(mtx, defer_lock);
-	while (1){
-		lock.lock();
-		while (flag != id){
-			cv.wait(lock);
-		}
-		//cout<<id;
-		//cout<<pthread_self()<<" ";
-		cout<<this_thread::get_id()<<" ";
-		flag++;
-		if (flag > 3){
-			flag = 1;
-			cout<<endl;
-			sleep(1);
-		}
-		cv.notify_all();
-		lock.unlock();
-	}
-}
-
+#include <stdio.h>
+extern char **environ;
 int main(void)
 {
-	thread threads[3];
-	flag=1;
-	for (int i=0; i<3; i++){
-		threads[i] = thread(print_id, i+1);
-	}
-		
-	for (auto &a : threads){
-		a.join();
+	char **env;
+	for (env=environ; *env!=NULL; env++){
+		printf("%s\n", *env);
 	}
 	return 0;
 }
