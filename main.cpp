235a8f4844f7ec68bf1311c0fdeded85ed1418ce
francis <yinghao_liu@163.com>
2018-10-26 16:47:07 +0800

    constexpr

--- main.cpp
+++ main.cpp
@@ -1,21 +1,11 @@
 #include <iostream>
-#include <string>
-#include <regex>
 using namespace std;
-
+constexpr long WEEK = 7+2;
+#define WEEK_DEF 7+2
 int main(void)
 {
-	/********more simple way to detect if match**********/
-	int n;
-	int tmp;
-	n=sscanf("2018-09-10", "%d-%d-%d", &tmp, &tmp, &tmp);
-	if (3 == n){
-		cout<<"match! n is "<<n<<endl;
-	}
-	/*******using c++11 regex**************/
-	if (regex_match("2018-09-10", regex("\\d{4}-\\d{2}-\\d{2}"))){
-		cout<<"match"<<endl;
-	}
+	cout<<WEEK * 2<<endl;
+	cout<<WEEK_DEF * 2<<endl;
 	return 0;
 }
 
